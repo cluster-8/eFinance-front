45fd54f7b5afdf794afdffeedb269546ee570e91
vinicius-hso <viniciushso@icloud.com>
2023-04-21 17:15:28 -0300

    Add types

---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+enum ServiceType {
+  PhysicalPerson = "Pessoa Física",
+  JuridicalPerson = "Pessoa Jurídica",
+  All = "Todos",
+}
+
+enum RankType {
+  LowerTariffs = "Menores tarifas",
+  HigherTariffs = "Maiores tarifas",
+}
+
+export { ServiceType, RankType };
