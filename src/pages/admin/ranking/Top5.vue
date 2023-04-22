35b40263eaeee99b532da43d0b1c43ce13ab91dd
vinicius-hso <viniciushso@icloud.com>
2023-04-22 16:56:07 -0300

    Refactor: add types

--- src/pages/admin/ranking/Top5.vue
+++ src/pages/admin/ranking/Top5.vue
@@ -1,32 +1,33 @@
 <template>
   <div>
     <h3 class="va-h3">Top 5</h3>
-    <p>Selecione abaixo o tipo de serviço, o serviço e a data do final do período</p>
+    <p>
+      Selecione abaixo o tipo de serviço, o serviço e a data do final do período
+    </p>
   </div>
 
-  <div style="width: 18.75rem; display: flex; margin-top: 1.25rem;">
-
+  <div style="width: 18.75rem; display: flex; margin-top: 1.25rem">
     <div class="mr-3">
       <va-select
         v-model="tipoServico"
         class="mt-3"
         label="Tipo de Serviço"
         :options="tipos"
-        style="width: 12.5rem;"
+        style="width: 12.5rem"
       />
     </div>
     <div class="mr-3">
       <va-select
-            placeholder="Selecione o serviço desejado"
-            v-model="selectedService"
-            class="mt-3"
-            label="Serviço"
-            :options="servicosFiltrados"
-            :text-by="(option) => option.nome"
-            style="width: 34rem;"
-            search-placeholder-text="Buscar"
-            searchable
-        />
+        placeholder="Selecione o serviço desejado"
+        v-model="selectedService"
+        class="mt-3"
+        label="Serviço"
+        :options="servicosFiltrados"
+        :text-by="(option) => option.nome"
+        style="width: 34rem"
+        search-placeholder-text="Buscar"
+        searchable
+      />
     </div>
 
     <div class="mr-3">
@@ -36,125 +37,158 @@
         :reset-on-close="false"
         label="Final do Periodo"
         mode="single"
-      />   
+        :format-date="(e: Date) => e.toLocaleDateString('pt-BR')"
+      />
     </div>
-
-    
   </div>
 
-  <div style="margin-top: 1.875rem;">
-    <div style="width: 10rem;">
-      <va-alert v-if="isError" icon="info" class="" style="width: 24rem;" :description="isError" />
+  <div style="margin-top: 1.875rem">
+    <div style="width: 10rem">
+      <va-alert
+        v-if="isError"
+        icon="info"
+        class=""
+        style="width: 24rem"
+        :description="isError"
+      />
     </div>
-    <Table v-if="!isError && isVisible" class="mt-3" style="width: auto" v-bind:topFive="topFive"></Table>
+    <Table
+      v-if="!isError && isVisible"
+      class="mt-3"
+      style="width: auto"
+      v-bind:topFive="topFive"
+    />
   </div>
-  
+
   <va-image
-    v-if = "!isVisible || isError"
+    v-if="!isVisible || isError"
     class=""
     :src="image"
-    style="width: 43.75rem; height: auto; margin-left: 25%; margin-top: -2rem; "
+    style="width: 43.75rem; height: auto; margin-left: 25%; margin-top: -2rem"
   />
-
 </template>
 
 <script lang="ts">
-  import { onMounted } from 'vue';
-  import Table from '../ranking/Top5Table.vue'
-  import { ref } from 'vue';
-  import api from '../../../services/api'
-  import image from "/finance-leaders-rafiki.png";
+import { ref, onMounted } from "vue";
 
-  const tipoServico = ref < "Pessoa Física" | "Pessoa Jurídica" | "Todos" > ("Todos");
-  const tipos = ["Pessoa Física", "Pessoa Jurídica", "Todos"]
-  const servicos = ref([])
-  const servicosFiltrados = ref()
-  const selectedService = ref()
-  const topFive = ref()
-  const formatedDate = ref()
-  const isError = ref(false)
-  const isVisible = ref(false)
+import Table from "../ranking/Top5Table.vue";
+import image from "/finance-leaders-rafiki.png";
 
-  const fetchServicos = async () => {
-    let response = await api.get("servicos")
-    servicos.value = response.data
-    servicosFiltrados.value = response.data
+import FinancialServices from "../../../services/FinancialServicesService";
+import Top5Service from "../../../services/Top5Service";
+
+import { FinancialService, ServiceType } from "../../../types";
+
+const tipoServico = ref<ServiceType>(ServiceType.All);
+const tipos: ServiceType[] = [
+  ServiceType.All,
+  ServiceType.PhysicalPerson,
+  ServiceType.JuridicalPerson,
+];
+const servicosFiltrados = ref<FinancialService[]>();
+const selectedService = ref<FinancialService>();
+const servicos = ref<FinancialService[]>([]);
+const isVisible = ref<boolean>(false);
+const isError = ref<boolean>(false);
+const topFive = ref<FinancialService[]>();
+const formatedDate = ref<string>();
+
+const fetchServicos = async () => {
+  const { data } = await FinancialServices.getAll();
+  servicos.value = sortServicosAsc(data);
+  servicosFiltrados.value = sortServicosAsc(data);
+};
+
+const fetchTop = async (id: string, date: string) => {
+  try {
+    const { data } = await Top5Service.getTop5ByServiceAndDate(id, date);
+    topFive.value = data;
+    isError.value = false;
+    isVisible.value = true;
+  } catch (error) {
+    isError.value = error.response.data.message;
   }
+};
 
-  const fetchTop = async (id, date, order) => {
-    try {
-      let response = await api.get(`tarifas/top-5/${id}?dataFim=${date}&page=4&order=${order}`)
-      topFive.value = response.data
-      isError.value = false
-      isVisible.value = true
-      
-    } catch (error) {
-      isError.value = error.response.data.message
+const formatDate = (date: Date) => {
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const year = date.getFullYear();
+  formatedDate.value = `${year}-${month}-${day}`;
+};
+
+const sortServicosAsc = (data: FinancialService[]) => {
+  data.sort((a, b) => {
+    const nameA = a.nome.toUpperCase();
+    const nameB = b.nome.toUpperCase();
+    if (nameA < nameB) {
+      return -1;
     }
-  }
-
-  const datePlusDay = (date, days) => {
-    const d = new Date(date);
-    d.setDate(d.getDate() + days);
-    return d;
-  };
-
-  const formatDate = (date) => {
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
-    const year = date.getFullYear();
-    formatedDate.value = `${year}-${month}-${day}`
-  }
-  
-  const nextWeek = datePlusDay(new Date(), 7);
-
-  export default {
-    name: "Variations",
-    components: {
-      Table
-    },
-    data() {
-      return {
-        tipoServico,
-        tipos,
-        servicos,
-        selectedService,
-        servicosFiltrados,
-        topFive,
-        formatedDate,
-        isError,
-        single: new Date(),
-        range: { start: new Date(), end: nextWeek },
-        image,
-        isVisible
-      };
-    },
-    watch: {
-      tipoServico(newValue) {
-        if(newValue === 'Pessoa Física'){
-          servicosFiltrados.value = servicos.value.filter(item => item.tipo === 'F')
-        }
-        else if(newValue === 'Pessoa Jurídica'){
-          servicosFiltrados.value = servicos.value.filter(item => item.tipo === 'J')
-        }
-        else if(newValue === 'Todos'){
-          servicosFiltrados.value = servicos.value
-        }
-        
-      },
-      single(newValue) {
-        formatDate(newValue)
-      },
-      selectedService(newValue){
-        fetchTop(newValue.id,formatedDate.value,'asc')
-      },
-    },
-    setup() {
-      onMounted(() => {
-        fetchServicos()
-        let date = new Date()
-        formatDate(date)
-      })
+    if (nameA > nameB) {
+      return 1;
     }
-  };
+    return 0;
+  });
+  return data;
+};
+
+export default {
+  name: "Variations",
+  components: {
+    Table,
+  },
+  data() {
+    return {
+      single: new Date(),
+      servicosFiltrados,
+      selectedService,
+      formatedDate,
+      tipoServico,
+      isVisible,
+      servicos,
+      topFive,
+      isError,
+      tipos,
+      image,
+    };
+  },
+  watch: {
+    tipoServico: {
+      handler: (newValue: ServiceType) => {
+        if (newValue === "Pessoa Física") {
+          servicosFiltrados.value = servicos.value.filter(
+            (item) => item.tipo === "F"
+          );
+        } else if (newValue === "Pessoa Jurídica") {
+          servicosFiltrados.value = servicos.value.filter(
+            (item) => item.tipo === "J"
+          );
+        } else if (newValue === "Todos") {
+          servicosFiltrados.value = servicos.value;
+        }
+      },
+      immediate: true,
+    },
+    single: {
+      handler: (newValue: Date) => {
+        formatDate(newValue);
+        if (selectedService.value)
+          fetchTop(selectedService.value.id, formatedDate.value);
+      },
+      immediate: true,
+    },
+    selectedService: {
+      handler: (newValue: FinancialService) => {
+        fetchTop(newValue.id, formatedDate.value);
+      },
+    },
+  },
+  setup() {
+    onMounted(() => {
+      fetchServicos();
+      let date = new Date();
+      formatDate(date);
+    });
+  },
+};
 </script>
