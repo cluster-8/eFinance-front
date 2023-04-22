30ac22928c0063e64fd2447250e4c2483be47cc7
vinicius-hso <viniciushso@icloud.com>
2023-04-22 18:15:17 -0300

    Fix: table column names

--- src/pages/admin/ranking/TarifasTable.vue
+++ src/pages/admin/ranking/TarifasTable.vue
@@ -6,9 +6,9 @@
           <table class="va-table va-table--striped va-table--hoverable">
             <thead>
               <tr>
-                <th class="align-table" style="width:15%">Posição</th>
-                <th class="align-table" style="width:70%">Instituição</th>
-                <th class="align-table" style="width:15%">Score</th>
+                <th class="align-table" style="width: 15%">Posição</th>
+                <th class="align-table" style="width: 70%">Instituição Financeira</th>
+                <th class="align-table" style="width: 15%">Score (média de tarifas)</th>
               </tr>
             </thead>
 
@@ -18,7 +18,7 @@
           :key="institution.id"
         >
           <td class="align-icon">
-            <div class="ranking-icon">#{{ index+1 }}</div></td>
+            <div class="ranking-icon">#{{ (index+1) + ((groupedProps.currentPage-1) * 20) }}</div></td>
           <td class="institution-name align-table">{{ institution.instituicao.nome }}</td>
           <td v-if="groupedProps.type === 'Pessoa Física'" class="maximum-value align-table">{{ institution.scorePf }}</td>
           <td v-else-if="groupedProps.type === 'Pessoa Jurídica'" class="maximum-value align-table">{{ institution.scorePj }}</td>
@@ -33,20 +33,21 @@
 </template>
 
 <script>
-  import { defineComponent } from "vue";
+import { defineComponent } from "vue";
 
   export default defineComponent({
     props: {
         groupedProps: {
           ranking: Object,
           type: Object,
+          currentPage: Number,
         }
       },
     data() {
       return {
       }
     },
-  });
+});
 </script>
 
 <style lang="scss">
@@ -61,7 +62,7 @@
 
   .ranking-icon {
     display: flex;
-    background-color: #0050C0;
+    background-color: #0050c0;
     text-align: center;
     height: 30px;
     width: 30px;
@@ -82,7 +83,6 @@
     align-items: center;
     justify-content: center;
     display: flex;
-
   }
 
   .maximum-value {
