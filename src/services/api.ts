99bcd29e53ef2b28ad0914983c22c0b09ea93c50
unknown <guilherme4garcia@gmail.com>
2023-04-29 13:54:54 -0300

    config api to run from .env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios, {AxiosInstance} from 'axios'
 
 const api: AxiosInstance = axios.create({
-  baseURL: "https://api-efinance.herokuapp.com/",
+  baseURL: import.meta.env.VITE_API_URL || "https://api-efinance.herokuapp.com/",
 })
 
 export default api
