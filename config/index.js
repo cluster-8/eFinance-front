1dcade2187470bd656f2f83a32c38fe16185cec0
Hariel <hariel@hotmail.com>
2023-06-06 08:47:56 -0300

    feat: remove chats files (deploy)

--- config/index.js
+++ config/index.js
@@ -4,7 +4,7 @@ const path = require('path')
 
 module.exports = {
   build: {
-    env: require('./prod.env'),
+    env: require('./.env'),
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
