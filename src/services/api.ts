57c0995801c65c22b01d142f81f8ee1f08670fa7
vinicius-hso <viniciushso@icloud.com>
2023-08-30 17:20:22 -0300

    Add redirect netilify

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios, {AxiosInstance} from 'axios'
 
 const api: AxiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL 
+  // baseURL: import.meta.env.VITE_BASE_URL 
+  baseURL: "/api"
 })
 
 export default api
